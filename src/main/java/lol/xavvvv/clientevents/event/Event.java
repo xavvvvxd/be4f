e4e7f6fb5989892873c9b1939947444bcd1ed23c
xavvvvxd <xavvvv.xd@gmail.com>
2024-09-29 13:45:07 +0100

    Implement simple EventManager.

---
+++ src/main/java/lol/xavvvv/clientevents/event/Event.java
@@ -0,0 +1,9 @@
+package lol.xavvvv.clientevents.event;
+
+import java.util.ArrayList;
+
+public abstract class Event<T extends Listener> {
+    public abstract void fire(ArrayList<T> listeners);
+
+    public abstract Class<T> getListenerClass();
+}
